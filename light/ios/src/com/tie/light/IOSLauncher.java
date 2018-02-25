fb3bac1dd3f01c20a630b1592c61174b24254c65
AlexAegis <alexaegis@gmail.com>
2018-02-25 18:10:36 +0100

    added readme

---
+++ light/ios/src/com/tie/light/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.tie.light;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.tie.light.LightMain;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LightMain(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
