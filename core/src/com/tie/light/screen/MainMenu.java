2f9d8e78b8ab4e6b45ce0267ba330de421e7c6a9
zolkol0077 <zolkol0077@users.noreply.github.com>
2018-04-29 03:17:40 +0200

    Added FreeType to gradle, modified MainMenu

--- core/src/com/tie/light/screen/MainMenu.java
+++ core/src/com/tie/light/screen/MainMenu.java
@@ -3,16 +3,17 @@ package com.tie.light.screen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -26,8 +27,17 @@ public class MainMenu implements Screen {
     private Viewport viewport;
     private OrthographicCamera camera;
     protected Skin skin;
+    BitmapFont font24;
+    Label label;
+    public AssetManager assets;
+    int slider_value;
 
     public MainMenu() {
+        assets = new AssetManager();
+        initFonts();
+
+
+        //skin.add("default-font", font24);
         skin = new Skin(Gdx.files.internal("shade/skin/uiskin.json"));
 
         batch = new SpriteBatch();
@@ -47,6 +57,9 @@ public class MainMenu implements Screen {
         //Stage should controll input:
         Gdx.input.setInputProcessor(stage);
 
+        final int width = Gdx.graphics.getWidth();
+        final int height = Gdx.graphics.getHeight();
+
         //Create Table
         Table mainTable = new Table();
         //Set table to fill stage
@@ -56,11 +69,22 @@ public class MainMenu implements Screen {
 
         //Create buttons
         TextButton playButton = new TextButton("Play", skin);
+        playButton.setSize(width*0.15f,height*0.1f);
+        playButton.setPosition(width*0.02f,height*0.79f);
         AtomicInteger playerCount = new AtomicInteger(1);
         Slider slider = new Slider(1, 4, 1, false, skin);
+        slider.setPosition(width*0.02f,height*0.65f);
+        slider_value = (int)slider.getValue();
         slider.addListener((Event event) -> { playerCount.set((int) slider.getValue()); return false; });
+        Gdx.app.log("slider.getvalue()", String.valueOf(slider.getValue()));
         TextButton exitButton = new TextButton("Exit", skin);
+        exitButton.setSize(width*0.15f,height*0.1f);
+        exitButton.setPosition(width*0.02f,height*0.52f);
 
+        label = new Label("Retro text: " + slider.getValue(),skin); //getValue nem változik
+        label.setSize(280,100);
+        label.setFontScale(3);
+        //label.setPosition(50,Gdx.graphics.getHeight()-120-100);
         //Add listeners to buttons
         playButton.addListener(new ClickListener(){
             @Override
@@ -75,7 +99,9 @@ public class MainMenu implements Screen {
             }
         });
 
+
         //Add buttons to table
+
         mainTable.add(playButton);
         mainTable.row();
         mainTable.add(slider);
@@ -83,14 +109,20 @@ public class MainMenu implements Screen {
         mainTable.add(exitButton);
 
         //Add table to stage
-        stage.addActor(mainTable);
+        //stage.addActor(mainTable); //table nem engedi resize olni a gombokat
+
+        //Add buttons to stage/menu
+        stage.addActor(playButton);
+        stage.addActor(slider);
+        stage.addActor(label);
+        stage.addActor(exitButton);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        label.setText("Retro text: " + slider_value);
         stage.act();
         stage.draw();
     }
@@ -120,6 +152,15 @@ public class MainMenu implements Screen {
     @Override
     public void dispose() {
         skin.dispose();
+    }
+
+    private void initFonts() {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Arcon.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        params.size = 24;
+        params.color = Color.BLACK;
+        font24 = generator.generateFont(params);
     }
 
 }
